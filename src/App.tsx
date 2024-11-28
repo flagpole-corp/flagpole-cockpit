707d07bc525d65e089d1c3a14aa9aa8589975c78
Vitor Boccio <vitorboccio@gmail.com>
2024-11-28 13:41:10 -0700

    providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppRouter } from './routes'
+import { AuthProvider } from './contexts/AuthContext'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App = (): JSX.Element => {
   return (
-    <>
-      <AppRouter />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
