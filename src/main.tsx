93f97d08b9e4fd994128a97304a506bf0484000a
Vitor Boccio <vitorboccio@gmail.com>
2024-12-30 15:05:24 -0700

    update react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './Reset.css'
 import App from './App'
 import CssBaseline from '@mui/material/CssBaseline'
 import { AppTheme } from './theming/AppTheme'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppTheme>
       <CssBaseline enableColorScheme />
+      <ToastContainer position="top-right" autoClose={5000} newestOnTop pauseOnHover />
       <App />
     </AppTheme>
   </StrictMode>
