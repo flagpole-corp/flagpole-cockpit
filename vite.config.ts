d2448cc7e4b187b4ebb81b15c0ce89daeba86dad
Vitor Boccio <vitorboccio@gmail.com>
2025-01-20 18:24:43 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  base: './',
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   },
 })
