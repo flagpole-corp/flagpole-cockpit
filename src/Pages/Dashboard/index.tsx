4d19dde1573b21a2501b6327c82f9de2e4a51c8d
Vitor Boccio <vitorboccio@gmail.com>
2025-01-27 13:53:57 -0700

    update links

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -2,7 +2,10 @@ import { Button, Grid2 as Grid } from '@mui/material'
 import { CardAlert } from '~/components'
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks'
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
+import { useNavigate } from 'react-router-dom'
 const Dashboard = (): JSX.Element => {
+  const navitate = useNavigate()
+
   return (
     <Grid spacing={2} container sx={{ width: '100%' }}>
       <Grid size={{ lg: 4, xs: 12 }}>
@@ -11,7 +14,14 @@ const Dashboard = (): JSX.Element => {
           icon={<LibraryBooksIcon />}
           text="Read our docs to learn how to easily integrate with your code"
           action={
-            <Button variant="contained" size="small" fullWidth>
+            <Button
+              variant="contained"
+              onClick={(): Window | null =>
+                window.open('https://docs.useflagpole.dev', '_blank', 'noopener,noreferrer')
+              }
+              size="small"
+              fullWidth
+            >
               Go to docs
             </Button>
           }
@@ -23,7 +33,7 @@ const Dashboard = (): JSX.Element => {
           icon={<RocketLaunchIcon />}
           text="Still not so sure how to get started? Head to our onboarding"
           action={
-            <Button variant="contained" size="small" fullWidth>
+            <Button onClick={(): void => navitate('/onboarding')} variant="contained" size="small" fullWidth>
               Go to onboarding
             </Button>
           }
