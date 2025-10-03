931597d2be36aab1fc4dcf84d10e9c4545d944d5
Vitor Boccio <vitorboccio@gmail.com>
2025-10-02 19:15:29 -0600

    fix page

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react'
 import type { NonIndexRouteConfig } from './config'
 
+const ActivityLogs = lazy(() => import('~/pages/activity-logs'))
 const Dashboard = lazy(() => import('~/pages/dashboard'))
 const Flags = lazy(() => import('~/pages/flags'))
 const Projects = lazy(() => import('~/pages/projects'))
